09c3830be2f6b8ee11c9f6634226f6a936345c60
huafua <huangfong@126.com>
2018-11-25 20:34:14 +0800

    a little bit change

--- js/novels.js
+++ js/novels.js
@@ -6,38 +6,36 @@ http.onreadystatechange=function(){
             novel_list.forEach(function(novel,index){
                 var novel_link="assets/"+novel;
                 var element=document.createElement("div");
-                element.style.position="relative";        
-                element.style.borderBottom="1px #ddd dashed";              
+                element.className="item-wrapper";
                 var icon=document.createElement("i");
-                icon.innerText=index+1;             
+                // icon.innerText=index+1;             
                 icon.className="icon";         
                 element.appendChild(icon);
                 var anchor=document.createElement("a");
                 anchor.className="novel_item";
                 anchor.href="javascript:void(0)";
-                anchor.onclick=function(){
+                element.onclick=function(){
                     http=new XMLHttpRequest();
                     http.onreadystatechange=function(){
                         if(this.readyState==4){
                             if(this.status==200){
                                 var panel=document.createElement("div");
                                 panel.className="panel";
-                                panel.style.width=document.body.offsetWidth+"px";
+                                panel.style.width=document.body.clientWidth+"px";
                                 panel.style.height=document.body.clientHeight+"px";
-                                // panel.innerText=this.responseText;
                                 var titlebar=document.createElement("div");
-                                titlebar.innerText=novel;
+                                titlebar.innerText=novel.split(".")[0];
                                 titlebar.className="titlebar";
                                 var btnback=document.createElement("div");
                                 btnback.className="btnback";
-                                btnback.innerHTML="&larr;";
+                                // btnback.innerHTML="&larr;";
                                 btnback.onclick=function(){
                                     document.body.removeChild(panel);
                                     panel=null;
                                 }
                                 titlebar.appendChild(btnback);
                                 var content=document.createElement("div");
-                                content.innerText=this.responseText;
+                                content.innerHTML=this.responseText.replace(/null/g,"");
                                 content.className="content";
                                 content.style.height=document.body.clientHeight-titlebar.offsetHeight+"px";
                                 panel.appendChild(titlebar);
