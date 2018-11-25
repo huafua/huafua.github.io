32fb4d70a723d767ff166538a701a7c15fa07a0d
huafua <huangfong@126.com>
2018-11-25 19:22:45 +0800

    now user can read on line

--- js/novels.js
+++ js/novels.js
@@ -14,7 +14,41 @@ http.onreadystatechange=function(){
                 element.appendChild(icon);
                 var anchor=document.createElement("a");
                 anchor.className="novel_item";
-                anchor.href=novel_link;
+                anchor.href="javascript:void(0)";
+                anchor.onclick=function(){
+                    http=new XMLHttpRequest();
+                    http.onreadystatechange=function(){
+                        if(this.readyState==4){
+                            if(this.status==200){
+                                var panel=document.createElement("div");
+                                panel.className="panel";
+                                panel.style.width=document.body.offsetWidth+"px";
+                                panel.style.height=document.body.clientHeight+"px";
+                                // panel.innerText=this.responseText;
+                                var titlebar=document.createElement("div");
+                                titlebar.innerText=novel;
+                                titlebar.className="titlebar";
+                                var btnback=document.createElement("div");
+                                btnback.className="btnback";
+                                btnback.innerHTML="&larr;";
+                                btnback.onclick=function(){
+                                    document.body.removeChild(panel);
+                                    panel=null;
+                                }
+                                titlebar.appendChild(btnback);
+                                var content=document.createElement("div");
+                                content.innerText=this.responseText;
+                                content.className="content";
+                                content.style.height=document.body.clientHeight-titlebar.offsetHeight+"px";
+                                panel.appendChild(titlebar);
+                                panel.appendChild(content);
+                                document.body.appendChild(panel);
+                            }
+                        }
+                    }
+                    http.open("get",novel_link,true);
+                    http.send();
+                }
                 anchor.setAttribute("target","_blank");
                 anchor.style.zIndex=100;
                 anchor.innerText=novel.split(".")[0];
